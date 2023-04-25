1cf569781d73f46055b494d2499437d6688e5d4e
stygian-lunar <jaiz.anq2000@gmail.com>
2023-04-25 09:51:44 +0530

    Fifth Commit

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -18,10 +18,11 @@ namespace dotnet_app.Services.CharacterService
 
         public CharacterService(IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
             
         }
-        //Add character
+        //Add character (POST)
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {   
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -31,7 +32,7 @@ namespace dotnet_app.Services.CharacterService
             serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
-        // Delete
+        // Delete (DELETE)
         public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -53,7 +54,7 @@ namespace dotnet_app.Services.CharacterService
             }
             return serviceResponse;
         }
-        //Get all
+        //Get all (GET)
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -61,16 +62,16 @@ namespace dotnet_app.Services.CharacterService
             serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
-        // Get By Id
+        // Get By Id (GET)
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
             return serviceResponse;
             
         }
-        // Update
+        // Update (PUT)
         public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
